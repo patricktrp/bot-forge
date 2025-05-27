79997395b770f5365ab490893c2fa9d994d708e5
Patrick Treppmann <patrick.treppmann97@gmail.com>
2025-05-27 23:24:09 +0200

    init backend

---
+++ api/src/main/java/dev/treppmann/bot_forge/BotForgeApplication.java
@@ -0,0 +1,12 @@
+package dev.treppmann.bot_forge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BotForgeApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(BotForgeApplication.class, args);
+	}
+
+}
---
+++ api/src/test/java/dev/treppmann/bot_forge/BotForgeApplicationTests.java
@@ -0,0 +1,15 @@
+package dev.treppmann.bot_forge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+@Import(TestcontainersConfiguration.class)
+@SpringBootTest
+class BotForgeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
